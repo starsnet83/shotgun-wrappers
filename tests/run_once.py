73857ef76eb214812cb2f7b70cdc507f0520c157
Joseph Bradley <joseph.kurata.bradley@gmail.com>
2014-01-09 17:11:02 -0800

    modifying shotgun to run logreg, but running into problems

---
+++ tests/run_once.py
@@ -0,0 +1,42 @@
+# Load a Matlab file, and run
+import unittest
+import numpy as np
+import scipy.io
+import sys
+sys.path.append('..')
+import shotgunpy
+
+data = scipy.io.loadmat("/Users/jbradley/data/lasso/arcene.mat")
+y = np.array(data['y'], dtype=np.float)
+A = data['A']
+
+solver = shotgunpy.ShotgunSolver()
+solver.set_use_offset(False)
+solver.set_maxIter(10)
+lam = .5
+sol = solver.solve_logreg(A,y,lam)
+sol.obj
+
+class TestPythonWrapper(unittest.TestCase):
+
+	def setUp(self):
+		self.solver = shotgunpy.ShotgunSolver()
+		self.solver.set_use_offset(False)
+
+	def test_lasso_simple(self):
+		A = np.array([[1, 1], [1, -1]], dtype=np.float)
+		y = np.array([1, 0], dtype=np.float)
+		lam = 0.5
+		sol = self.solver.solve_lasso(A, y, lam)
+		self.assertEqual(sol.w[0], 0.25)
+		self.assertEqual(sol.w[1], 0.25)
+
+	def test_lasso_offset(self):
+		self.solver.set_use_offset(True)
+		A = np.eye(3)
+		y = np.array([1, 1, 1], dtype=np.float)
+		sol = self.solver.solve_lasso(A, y, 0.5)
+		self.assertEqual(sol.offset, 1.)
+
+if __name__ == '__main__':
+	unittest.main()
